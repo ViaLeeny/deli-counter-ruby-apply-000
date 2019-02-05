0b3e783342dbc906fd4c89f6e4c19333a081ab9e
Ayleen  Nazario <ayleen.nazario@gmail.com>
2019-02-05 07:42:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,33 +1,31 @@
 katz_deli = []
+$counter = 1
 
 def line (array)
   if array.length >= 1
     the_line = []
-    counter = 1
     array.each {
-    |person|
-    the_line.push("#{counter}. #{person}")
-     counter += 1
+     |person|
+     the_line.push("#{counter}. #{person}")
+     $counter += 1
     }
    puts "The line is currently: #{the_line.join(" ")}"
   else  
-  puts "The line is currently empty."
-end
+    puts "The line is currently empty."
+  end
 end
 
 def take_a_number (array, name)
-array.push(name)
-position = array.index(name) 
-puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+  array.push(name)
+  position = array.index(name) 
+  puts "Welcome, #{name}. You are number #{$counter} in line."
 end
 
 def now_serving (array)
-  
-  if array.empty? == true
+  if $counter == 0
     puts "There is nobody waiting to be served!"
-  elsif array.empty? == false
+  else
      puts "Currently serving #{array.shift}."
-     
-end
   end
+end
 
